af92af32bb77432fa576cab5933ebd1706cfc8c4
Luděk Roleček <ludek@rolecek.cz>
2023-04-29 10:52:42 +0200

    Vytvorene komponenty

---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import './style.css';
+
+const Footer = () => {
+	const element = document.createElement('footer');
+	element.innerHTML =  `
+		<div class="container">
+			<div class="footer__content">
+				Café Lóra je tréningový projekt v rámci Czechitas kurzu JavaScript 2
+			</div>
+		</div>
+	`;
+
+	return element;
+}
+
+export default Footer;
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import './style.css';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Banner from './components/Banner';
+import Menu from './components/Menu';
+import Gallery from './components/Gallery';
+import Contact from './components/Contact';
+
+console.log('funguju!');
+
+const pageElement = document.createElement('div');
+pageElement.classList.add('page');
+
+const main = document.createElement('main');
+main.append(Banner(), Menu(), Gallery(), Contact());
+
+pageElement.append(Header(), main, Footer());
+
+document.querySelector('#app').append(pageElement);
+
+
